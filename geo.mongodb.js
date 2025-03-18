052586d60ba63f9d354b02c319cd1f269066c122
Isabel Maito <maitoisabel@gmail.com>
2025-03-17 21:46:35 -0300

    Add: geolocalização de  municipios

--- geo.mongodb.js
+++ geo.mongodb.js
@@ -43,3 +43,54 @@ db.estados.find({
         }
     }
 },{nome: 1, _id: 0}) //6368.1 é o raio médio da terra em km
+
+
+
+//Para Municipios
+
+use('estoque')
+db.municipios.find().forEach(function (municipio) {
+    db.municipios.updateOne(
+        {_id: municipio._id},
+        { $set: {
+            local: {
+                type: 'Point',
+                coordinates: [municipio.longitude, municipio.latitude]
+                }
+            },        
+        $unset: {latitude: "", longitude: ""}
+        }
+        )
+})
+use('estoque')
+db.municipios.find({}, {local:1})
+
+use('estoque')
+db.municipios.createIndex({local: '2dsphere'})
+
+use('estoque')
+db.municipios.find({
+    local: {
+        $near: {
+            $geometry: {
+                type: 'Point',
+                coordinates: [-47.4496159092281, -23.53138370990615] //Fatec
+            },
+            $maxDistance: 50000 //em metros - 500km
+        }
+    }
+},{nome: 1, _id: 0})
+
+
+use('estoque')
+db.municipios.find({
+    local: {
+        $geoWithin: {
+            $centerSphere: [[-47.4495, -23.5313], 
+            20 / 6378.1] //raio em radianos
+        }
+    }
+},{nome: 1, _id: 0})
+
+
+
