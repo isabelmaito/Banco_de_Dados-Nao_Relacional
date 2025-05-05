0d39a2f09ad929355eec8e51c60437623b05a41e
Isabel Maito <maitoisabel@gmail.com>
2025-05-05 19:44:11 -0300

    Listando usuários

--- MongoGeo/api/models/usuarios.mongodb.js
+++ MongoGeo/api/models/usuarios.mongodb.js
@@ -7,4 +7,9 @@ db.usuarios.insertOne({
     'tipo': 'Cliente',
     'avatar': 'https://ui-avatars.com/api?name=Maria+Jose&background=F00&color=FFF'
 })
+
 //Criando um indice único para o e-mail
+db.usuarios.createIndex({'email':1}),({unique:true})
+
+//Listando os usuários
+db.usuarios.find({},{senha:0})
