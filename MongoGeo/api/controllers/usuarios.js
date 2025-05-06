5c0187742231e894b905ccf86cc35b598aba1323
Isabel Maito <maitoisabel@gmail.com>
2025-05-05 21:25:51 -0300

    criptografia da senha e salvando o usuário

--- MongoGeo/api/controllers/usuarios.js
+++ MongoGeo/api/controllers/usuarios.js
@@ -7,4 +7,16 @@ export const insereUsuario = async(req, res) =>{
     //bcryptjs -> criptografar o conteúdo
     //jsonwebtoken -> para gerar o jwt
     //npm i bcryptjs jsonwebtoken
+
+    //criptografia da senha
+    const salt = await bcrypt.genSalt(10) //10 rodadas de processamento de hash
+    req.body.senha = await bcrypt.hash(req.body.senha, salt)
+
+    //salvando o usuário
+    const db = req.app.locals.db
+    await db.collection('usuarios')
+        .insertOne(req.body)
+        .then(result => res.status(201).send(result)
+            .catch(err => Response.status(400).json(err))
+        )
 }
