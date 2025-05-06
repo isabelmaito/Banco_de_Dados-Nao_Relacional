74fc4b18847404aee01acfb5f300a1b56b0e5673
Isabel Maito <maitoisabel@gmail.com>
2025-05-05 21:13:49 -0300

    importando as libs

--- MongoGeo/api/controllers/usuarios.js
+++ MongoGeo/api/controllers/usuarios.js
@@ -1,4 +1,6 @@
 import { ObjectId } from 'mongodb'
+import bcrypt from bcryptjs
+import jwt from 'jsonwebtoken'
 
 export const insereUsuario = async(req, res) =>{
     req.body.avatar = `https://ui-avatars.com/api/?name=${req.body.nome.replace}(/ /g, '+'))&background=F00&color=FFF`
