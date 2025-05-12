846286aaed4226e098a79ab6b3fd51628380ee14
Isabel Maito <maitoisabel@gmail.com>
2025-05-12 20:08:50 -0300

    geração de token

--- MongoGeo/api/controllers/usuarios.js
+++ MongoGeo/api/controllers/usuarios.js
@@ -29,7 +29,38 @@ export const efetuaLogin = async (req,res) => {
         let usuario = await db.collection('usuarios').find({email}).limit(1).toArray()
         //se o array estiver vazio, é porque não tem
         if(!usuario.lengt){
-            
+            return res.status(404).json({
+                errors: [{
+                    value: `${email}`,
+                    msg: `O email ${email} não está cadastrado`,
+                    param: 'email'
+                }]
+            })
+
         }
+        //verificando a senha
+        const isMatch = await bcrypt.compare(senha, usuario[0].senha)
+        if(!isMatch)
+            return res.status(403).json({//forbidden
+                errors: [{
+                    value: 'senha',
+                    msg: 'a senha informada está incorreta',
+                    param: 'senha'
+                }]
+        })
+        //se tudo ok, iremos gerar o token JWT
+        jwt.sign(
+            {usuario: {id: usuario[0]._id}},
+            process.env.SECRET_KEY,
+            {expiresIn: process.env.EXPIRES_IN},
+            (err, token) => {
+                if(err) throw errres.status(200).json({
+                    acess_token: token,
+                    msg: 'Login efetuado com sucesso'
+                })
+            }
+        )
+    } catch(e){
+        console.error(e)
     }
 }
