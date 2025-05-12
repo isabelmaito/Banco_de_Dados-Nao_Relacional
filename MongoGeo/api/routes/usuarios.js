c87aa553b8076239a3a1a22b03945b7683e21c88
Isabel Maito <maitoisabel@gmail.com>
2025-05-12 19:46:00 -0300

    validando login nas rotas

--- MongoGeo/api/routes/usuarios.js
+++ MongoGeo/api/routes/usuarios.js
@@ -5,6 +5,9 @@ import {validateUsuario} from '../middlewares/validations.js'
 const router = express.Router()
 //cria novo usuário
 router.post('/', validateUsuario, insereUsuario)
+//valida o login
+router.post('/login', efetuaLogin)
+
 
 export default router
 
